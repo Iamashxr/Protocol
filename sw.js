687435ef409ec526c1190861aaa3e9794fa20928
Iamashxr <163728546+Iamashxr@users.noreply.github.com>
2026-04-17 16:01:42 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'protocol-v2';
+const ASSETS = [
+  '/Protocol/',
+  '/Protocol/index.html',
+  '/Protocol/manifest.json',
+  '/Protocol/icon-192.png',
+  '/Protocol/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+        if (event.request.method === 'GET') {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, clone);
+          });
+        }
+        return response;
+      });
+    }).catch(() => {
+      return caches.match('/Protocol/index.html');
+    })
+  );
+});
